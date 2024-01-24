c16b7ce29f6031a192428700680c92d3a595ec01
colly-m <sindanicolly@gmail.com>
2024-01-24 12:06:27 -0800

    Get nginx through puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,28 +1,57 @@
-# Script to install nginx using puppet
+# Define a Puppet class for installing and configuring Nginx
+class nginx_installation {
 
-package { 'nginx':
-  ensure        => 'present',
-  listen_ports  =>  ['80'],
+  # Ensure Nginx package is installed
+  package { 'nginx':
+    ensure => 'present',
+  }
+
+  # Manage the Nginx default configuration file
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    source  => 'puppet:///modules/nginx_installation/default',
+    notify  => Exec['nginx_restart'],
+  }
+
+  # Manage the HTML file
+  file { '/var/www/html/index.html':
+    ensure  => file,
+    content => "Hello World!\n",
+  }
+
+  # Manage Nginx service
+  service { 'nginx':
+    ensure    => 'running',
+    enable    => true,
+    subscribe => File['/etc/nginx/sites-available/default'],
+  }
+
+  # Restart Nginx when the configuration changes
+  exec { 'nginx_restart':
+    command     => 'sudo service nginx restart',
+    refreshonly => true,
+  }
+
 }
 
-service { 'nginx':
-  ensure        => 'running',
-  enable        => true,
-  subscribe     => Package['nginx'],
-}
+# Include the class to apply it
+include nginx_installation
 
-file { '/var/www/html/index.html':
-  ensure        => 'file',
-  content       => 'Hello World!',
-}
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
 
-file { '/etc/nginx/sites-available/default':
-  ensure        => 'file',
-  content       => template('path/to/your/nginx_config.erb'),
-  notify        => Exec['nginx-restart'],
-}
+    root /var/www/html;
 
-exec { 'nginx-restart':
-  command       => 'service nginx restart',
-  refreshonly   => true,
+    index index.html index.htm;
+
+    server_name _;
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4/;
+    }
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
 }
