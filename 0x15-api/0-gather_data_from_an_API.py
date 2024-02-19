814be3ada578a3ccf250fb23180b5fd3a3710ada
colly-m <sindanicolly@gmail.com>
2024-02-19 15:59:21 -0800

    API data gathering fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,41 +3,23 @@
 Script to using this REST API, for a given employee ID,
 returns information about his/her TODO list progress
 """
-import sys
-import requests
+from requests import get
+from sys import argv
 
 
-def fetch_todo_list_progress(employee_id):
-    main_url = "https://jsonplaceholder.typicode.com"
-    todo_url = f"{main_url}/todos?userId={employee_id}"
-    user_url = f"{main_url}/users/{employee_id}"
+if __name__ == '__main__':
+    base_url = 'https://jsonplaceholder.typicode.com'
+    todo_url = base_url + "/user/{}/todos".format(argv[1])
+    name_id = base_url + "/users/{}".format(argv[1])
+    todo_outcome = get(todo_url).json()
+    name_outcome = get(name_id).json()
 
-    try:
-        user_response = requests.get(user_url)
-        user_data = user_response.json()
-        employee_name = user_data['name']
-
-        todo_response = requests.get(todo_url)
-        todo_data = todo_response.json()
-
-        total_tasks = len(todo_data)
-        completed_tasks = [todo['title'] for todo in todo_data
-                           if todo['completed']]
-        num_completed_tasks = len(completed_tasks)
-
-        print(f"Employee {employee_name} is done with tasks("
-              f"{num_completed_tasks}/{total_tasks}): ")
-        for task_title in completed_tasks:
-            print(f"\t{task_title}")
-
-    except requests.exceptions.RequestException as e:
-        print("Error fetching data:", e)
-        sys.exit(1)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-    employee_id = int(sys.argv[1])
-    fetch_todo_list_progress(employee_id)
+    todo_num = len(todo_outcome)
+    todo_complete = len([todo for todo in todo_outcome
+                         if todo.get("completed")])
+    name = name_outcome.get("name")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, todo_complete, todo_num))
+    for todo in todo_outcome:
+        if (todo.get("completed")):
+            print("\t {}".format(todo.get("title")))
