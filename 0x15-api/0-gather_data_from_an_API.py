253843cc5caa274e496064a431b870b2619be826
colly-m <sindanicolly@gmail.com>
2024-02-19 13:16:38 -0800

    API data gathering

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Script to using this REST API, for a given employee ID,
+returns information about his/her TODO list progress
+"""
+import sys
+from requests import get
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    usr_id = get(url + 'users/{}'.format(sys.argv[1])).json()
+    to_do = get(url + 'todos', params={'userId': sys.argv[1]}).json()
+
+    completed = [title.get("title") for title in to_do if
+                 title.get('completed') is True]
+    print(completed)
+    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
+                                                          len(completed),
+                                                          len(to_do)))
+    [print("\t {}".format(title)) for title in completed]
