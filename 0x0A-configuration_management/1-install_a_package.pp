cb2167de3869dfc6a442bfdaa23a82fadadd6b6d
colly-m <sindanicolly@gmail.com>
2024-02-19 17:06:52 -0800

    untracked

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # Installation of flask through pip3
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip
+package { 'flask':
+  ensure   => '2.1.0',
   provider => 'pip3',
+}
+~
+3',
 }
