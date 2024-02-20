94af8f221e2e5b66caecc8aeda02a73de4491201
colly-m <sindanicolly@gmail.com>
2024-02-19 17:30:08 -0800

    API export fixes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,20 +1,41 @@
 #!/usr/bin/python3
-"""script using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
-from requests import get
+"""
+Script to export data in the csv format, then records tasks
+"""
+import requests
 import sys
-import json
 
 
-if __name__ == '__main__':
-    base_url = 'https://jsonplaceholder.typicode.com/'
-    user_id = get(base_url + 'users/{}'.format(sys.argv[1])).json()
-    todo = get(base_url + 'todos', params={'userId': sys.argv[1]}).json()
+def fetch_todo_list_progress(employee_id):
+    base_url = "https://jsonplaceholder.typicode.com"
+    todos_url = f"{base_url}/todos?userId={employee_id}"
+    user_url = f"{base_url}/users/{employee_id}"
 
-    completed = [title.get("title") for title in todo if
-                 title.get('completed') is True]
-    print(completed)
-    print("Employee {} is done with tasks({}/{}):".format(user_id.get("name"),
-                                                          len(completed),
-                                                          len(todo)))
-    [print("\t {}".format(title)) for title in completed]
+    try:
+        user_response = requests.get(user_url)
+        user_data = user_response.json()
+        employee_name = user_data['name']
+
+        todos_response = requests.get(todos_url)
+        todos_data = todos_response.json()
+
+        total_tasks = len(todos_data)
+        ended_tasks = [todo['title'] for todo in todos_data
+                       if todo['completed']]
+        num_completed_tasks = len(ended_tasks)
+
+        print(f"Employee {employee_name} is done with tasks("
+              f"{num_completed_tasks}/{total_tasks}): ")
+        for task_title in ended_tasks:
+            print(f"\t{task_title}")
+
+    except requests.exceptions.RequestException as e:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+    employee_id = int(sys.argv[1])
+    fetch_todo_list_progress(employee_id)
