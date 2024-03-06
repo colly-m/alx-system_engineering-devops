bd089ce6347e8c67c74627afb148315e458665aa
colly-m <sindanicolly@gmail.com>
2024-03-06 16:08:07 +0000

    Amigo Strace

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,10 @@
+# puppet manuscript to replace a line in a file on a server
+
+$file_to_edit = "/var/www/html/wp-settings.php"
+
+#Fixing line containing "phpp" with "php"
+
+exec { 'fix_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => "/usr/local/bin/:/bin/"
+}
